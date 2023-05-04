5edf7c517e5011f00f255433b443dc22373baa1f
sahil-roam <sahil@roam.ai>
2023-05-04 15:45:00 +0530

    added headless js with toast

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Toast from 'react-native-simple-toast';
 
+
+function registerListener(){
+    console.log('index register')
+    Roam.startListener('location', locations => {
+        Toast.show(JSON.stringify(locations), Toast.LONG);
+    })
+}
+
+AppRegistry.registerHeadlessTask('RoamHeadlessService', registerListener());
 AppRegistry.registerComponent(appName, () => App);
