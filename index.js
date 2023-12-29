bf90524d3d3115fb0e2109dcbbf99d2d8dede75b
sahil-roam <97589523+sahil-roam@users.noreply.github.com>
2023-12-29 15:22:38 +0530

    update roam version

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Toast from 'react-native-simple-toast';
+import Roam from 'roam-reactnative';
 
 
 function registerListener(){
